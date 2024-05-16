4ee0389c4b440a41fa3b94992e54401d8343ca37
Anurag Gawande <anuraggawande2001@gmail.com>
2024-05-16 13:13:33 +0530

    Uploading Contact Book

---
+++ contact_book.py
@@ -0,0 +1,23 @@
+from contact import Contact
+
+class ContactBook:
+    def __init__(self):
+        self.contacts=[]
+    
+    def add_contact(self,contact):
+        self.contacts.append(contact)
+    
+    
+    def display_all_contact(self):
+        for contact in self.contacts:
+            contact.display_contact()
+            print("--------------------------") 
+    
+    
+    def  search_contact(self,name):
+        for contact in self.contacts:
+            if contact.name.lower()==name.lower():
+                return contact
+        else:
+            return None
+    
